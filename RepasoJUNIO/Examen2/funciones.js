f376bf6fdc752953a16f6c7bfc3401ec25b4957a
Geovanny <geova.jb99@hotmail.com>
2026-03-19 21:11:32 +0100

    php ok y funciones empezaddo

---
+++ RepasoJUNIO/Examen2/funciones.js
@@ -0,0 +1,28 @@
+
+
+
+let container;
+let container2;
+let container3;
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+document.body.style.display="flex";
+document.body.style.justifyContent="center";
+document.body.style.alignItems="center";
+document.body.style.height="100vh";
+
+container=document.createElement('div');
+container.id="general";
+container.style.width="300px";
+container.style.height="500px";
+container.style.border="solid 2px black";
+container.style.display="flex";
+container.style.justifyContent="center";
+container.style.alignItems="center";
+document.body.appendChild(container);
+
+
+
+
+} )
